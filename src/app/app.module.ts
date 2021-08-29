7b13fd6465111d20fc87ee5aa139624e12739c34
Mihai Ungureanu <u.mihai97@gmail.com>
2021-08-30 01:43:32 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { SpeechRecognition} from '@ionic-native/plugins/speech-recognition/ngx'; 
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,7 +12,11 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SpeechRecognition
+  ],
+  bootstrap: [AppComponent]
 })
+
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component,ChangeDetectorRef } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/plugins/speech-recognition/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  matches : string[];
+  isRecording: boolean;
+  button : string;
 
-  constructor() {}
+  constructor(private speechRecognition : SpeechRecognition,private cd: ChangeDetectorRef) {}
+  //constructor(){}
 
+   stopListening() {
+     this.speechRecognition.stopListening().then(() => {
+       this.isRecording = false;
+     });
+    this.button = "stopListening was pressed"
+   }
+
+   getPermission() {
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission();
+        }
+      });
+      this.button = "getPermission was pressed"
+  }
+
+   startListening() {
+    this.speechRecognition.startListening().subscribe(matches => {
+      this.matches = matches;
+      this.cd.detectChanges();
+    });
+    this.isRecording = true;
+
+    this.button = "startListening was pressed"
+   }
 }
